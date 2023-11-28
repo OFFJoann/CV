bf760172b8e660df18ebf2b85e841de781f49b78
OFFJoann <jduque@NADAR.LOCAL>
2023-11-28 16:27:46 -0500

    Se añadio todos los archivos para la hoja de vida

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap = Bootstrap(app)
+
+@app.route('/')
+def header():
+    return render_template('header.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
