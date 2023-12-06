b611f285f847dfd407b474acf735015e904b49b7
OFFJoann <jduque@NADAR.LOCAL>
2023-12-06 13:51:03 -0500

    Se modificaron los colores en el header de la pagina

--- static/js/estudios.js
+++ static/js/estudios.js
@@ -11,7 +11,7 @@ function mostrarestudios() {
         <div class="contenedor" style="top: auto">
             <img src="../static/images/diploma.png" class="icons_links">
             <a class="texto_alineado" href="https://drive.google.com/file/d/1LmFCsiPwkuEgZgXhDQP6CXIyorJn--Zv/view?usp=drive_link" target="_blank" style="text-decoration: none; pointer-events: auto;">
-                <p style="color: blueviolet;"><b>Diploma Bachiller</b></p>
+                <p style="color: #6b4d87;"><b>Diploma Bachiller</b></p>
             </a>
         </div>
         `;
@@ -20,28 +20,28 @@ function mostrarestudios() {
     <div class="contenedor" style="top: auto">
         <img src="../static/images/diploma.png" class="icons_links">
         <a class="texto_alineado" href="https://drive.google.com/file/d/1HB29bCxcQL7hYbLh1brOXC0hKAMs_7bN/view?usp=drive_link" target="_blank" style="text-decoration: none; pointer-events: auto;">
-            <p style="color: blueviolet;"><b>Diploma Tecnica</b></p>
+            <p style="color: #6b4d87;"><b>Diploma Tecnica</b></p>
         </a>
     </div>
         
     <div class="contenedor" style="top: auto">
         <img src="../static/images/diploma.png" class="icons_links">
         <a class="texto_alineado" href="https://drive.google.com/file/d/10G5Aq81Bq6A7Ffdu96bIMNJIJVC-v7qS/view?usp=sharing" target="_blank" style="text-decoration: none; pointer-events: auto;">
-            <p style="color: blueviolet;"><b>Ingles #1</b></p>
+            <p style="color: #6b4d87;"><b>Ingles #1</b></p>
         </a>
     </div> 
 
     <div class="contenedor" style="top: auto">
         <img src="../static/images/diploma.png" class="icons_links">
         <a class="texto_alineado" href="https://drive.google.com/file/d/11iYqOhsxm2SJ1mXG10ThA7IyrlJabqlR/view?usp=sharing" target="_blank" style="text-decoration: none; pointer-events: auto;">
-            <p style="color: blueviolet;"><b>Ingles #2</b></p>
+            <p style="color: #6b4d87;"><b>Ingles #2</b></p>
         </a>
     </div>
 
     <div class="contenedor" style="top: auto">
         <img src="../static/images/diploma.png" class="icons_links">
         <a class="texto_alineado" href="https://drive.google.com/file/d/1bt1IkVdg2BQljgzQj9eEoy2F20ksxkft/view?usp=sharing" target="_blank" style="text-decoration: none; pointer-events: auto;">
-            <p style="color: blueviolet;"><b>Ingles #3</b></p>
+            <p style="color: #6b4d87;"><b>Ingles #3</b></p>
         </a>
     </div>
         
@@ -51,49 +51,49 @@ function mostrarestudios() {
     <div class="contenedor" style="top: auto">
         <img src="../static/images/desarrollo_icon.png" class="icons_links">
         <a class="texto_alineado" href="https://drive.google.com/file/d/13uZbJdc6vDbNMC7gBgXrpg8_wXRa27g1/view?usp=drive_link" target="_blank" style="text-decoration: none; pointer-events: auto;">
-            <p style="color: blueviolet;"><b>Diploma Terminal y linea de comandos</b></p>
+            <p style="color: #6b4d87;"><b>Diploma Terminal y linea de comandos</b></p>
         </a>
     </div>
 
     <div class="contenedor" style="top: auto">
         <img src="../static/images/desarrollo_icon.png" class="icons_links">
         <a class="texto_alineado" href="https://drive.google.com/file/d/18ESNs0dXdHcTCQwrZxocUPJ839-pNSJo/view?usp=sharing" target="_blank" style="text-decoration: none; pointer-events: auto;">
-            <p style="color: blueviolet;"><b>Diploma Fundamentos de Python</b></p>
+            <p style="color: #6b4d87;"><b>Diploma Fundamentos de Python</b></p>
         </a>
     </div>
 
     <div class="contenedor" style="top: auto">
     <img src="../static/images/desarrollo_icon.png" class="icons_links">
     <a class="texto_alineado" href="https://drive.google.com/file/d/1OFCfOlUXmk_wgxHhI9rY1Hpcv2dBsHse/view?usp=sharing" target="_blank" style="text-decoration: none; pointer-events: auto;">
-        <p style="color: blueviolet;"><b>Diploma Python PIP y entornos virtuales</b></p>
+        <p style="color: #6b4d87;"><b>Diploma Python PIP y entornos virtuales</b></p>
     </a>
     </div>
 
     <div class="contenedor" style="top: auto">
         <img src="../static/images/desarrollo_icon.png" class="icons_links">
         <a class="texto_alineado" href="https://drive.google.com/file/d/1CA_Iiq1G-eLHQv4_oh_x_TI6FZC8gLJ8/view?usp=sharing" target="_blank" style="text-decoration: none; pointer-events: auto;">
-            <p style="color: blueviolet;"><b>Diploma Python: Comprehensions, funciones y manejo de errores </b></p>
+            <p style="color: #6b4d87;"><b>Diploma Python: Comprehensions, funciones y manejo de errores </b></p>
         </a>
     </div>
 
     <div class="contenedor" style="top: auto">
         <img src="../static/images/desarrollo_icon.png" class="icons_links">
         <a class="texto_alineado" href="https://drive.google.com/file/d/1KFN79x428pBA8Odp-A7646JL5WrkfT6T/view?usp=sharing" target="_blank" style="text-decoration: none; pointer-events: auto;">
-            <p style="color: blueviolet;"><b>Diploma Git y GitHub</b></p>
+            <p style="color: #6b4d87;"><b>Diploma Git y GitHub</b></p>
         </a>
     </div>
 
     <div class="contenedor" style="top: auto">
         <img src="../static/images/desarrollo_icon.png" class="icons_links">
         <a class="texto_alineado" href="https://drive.google.com/file/d/15lacUP05IQVHwa5dXuGSlQ0jokjkZIkg/view?usp=sharing" target="_blank" style="text-decoration: none; pointer-events: auto;">
-            <p style="color: blueviolet;"><b>Diploma FastAPI</b></p>
+            <p style="color: #6b4d87;"><b>Diploma FastAPI</b></p>
         </a>
     </div>
 
     <div class="contenedor" style="top: auto">
         <img src="../static/images/desarrollo_icon.png" class="icons_links">
         <a class="texto_alineado" href="https://drive.google.com/file/d/1QypQL6dOvB4MDbZ9rBk5wBM_icUZ87vG/view?usp=sharing" target="_blank" style="text-decoration: none; pointer-events: auto;">
-            <p style="color: blueviolet;"><b>Diploma introduccion al backend</b></p>
+            <p style="color: #6b4d87;"><b>Diploma introduccion al backend</b></p>
     </a>
     </div>
 
--- static/js/experiencia.js
+++ static/js/experiencia.js
@@ -7,13 +7,13 @@ function mostrarseleccion() {
     if (seleccion.value == "Teleperformace") {resultado.innerHTML= '';
         resultado.innerHTML = `
     <ul>
-        <li><h1 style="font-size: medium; color: blueviolet;"><b>Teleperformace 2019-2020</b></h1></li>
+        <li><h1 style="font-size: medium; color: #92aecb;"><b>Teleperformace 2019-2020</b></h1></li>
             <ul>
-            <li><p style="font-size: small;">Analista en sistemas</p></li>
+            <li><p style="font-size: small; color: #669787;">Analista en sistemas</p></li>
             </ul>
-        <li><h2 style="font-size: medium; color: blueviolet;">Funciones</h2></li>
+        <li><h2 style="font-size: medium; color: #92aecb;">Funciones</h2></li>
             <ul>
-            <li><p style="font-size: small;">Solución de problemas de aplicativos empresariales, servidores, telefonia, y de red, administración de active directory.</p></li>
+            <li><p style="font-size: small; color: #669787;">Solución de problemas de aplicativos empresariales, servidores, telefonia, y de red, administración de active directory.</p></li>
             </ul>
     </ul>
     
@@ -21,13 +21,13 @@ function mostrarseleccion() {
     } else if (seleccion.value == "Creaciones Nadar S.A") {
         resultado.innerHTML =`
     <ul>
-        <li><h1 style="font-size: medium; color: blueviolet;"><b>Creaciones Nadar S.A 2021-2023</b></h1></li>
+        <li><h1 style="font-size: medium; color: #92aecb;"><b>Creaciones Nadar S.A 2021-2023</b></h1></li>
             <ul>
-            <li><p style="font-size: small;">Analista en sistemas</p></li>
+            <li><p style="font-size: small; color: #669787;">Analista en sistemas</p></li>
             </ul>
-        <li><h2 style="font-size: medium; color: blueviolet;">Funciones</h2></li>
+        <li><h2 style="font-size: medium; color: #92aecb;">Funciones</h2></li>
             <ul>
-            <li><p style="font-size: small;">Administrar infraestructura, dar soporte a nuestros clientes, implementar nuevas aplicaciones, metodologías de trabajo y establecer <br>procedimientos para asegurar que la infraestructura sea la mas optima, desarrollo y elaboración de políticas de seguridad basadas <br>en la normal ISO 27001, realizar campañas de phishing, manejo de Windows server y Linux, implementación de políticas en Fortinet <br>y Mikrotik, elaboración de VPN ipsec, SSL, administración de backups de servidores, uso de nmap para verificar puertos abiertos en la red.</p></li>
+            <li><p style="font-size: small; color: #669787;">Administrar infraestructura, dar soporte a nuestros clientes, implementar nuevas aplicaciones, metodologías de trabajo y establecer <br>procedimientos para asegurar que la infraestructura sea la mas optima, desarrollo y elaboración de políticas de seguridad basadas <br>en la normal ISO 27001, realizar campañas de phishing, manejo de Windows server y Linux, implementación de políticas en Fortinet <br>y Mikrotik, elaboración de VPN ipsec, SSL, administración de backups de servidores, uso de nmap para verificar puertos abiertos en la red.</p></li>
             </ul>
     </ul>
         `
